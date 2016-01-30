80352882a91ccb88001f06de6ab96f50ded3d9c7
Vladislav <v.d.gushin@live.ru>
2016-01-30 14:20:41 +0300

    Update App.xaml.cs

--- TestUniversalApp/App.xaml.cs
+++ TestUniversalApp/App.xaml.cs
@@ -49,7 +49,7 @@ namespace TestUniversalApp
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
-            Frame rootFrame = Window.Current.Content; //as //Frame;
+            Frame rootFrame = Window.Current.Content as Frame;
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
             if (rootFrame == null)
