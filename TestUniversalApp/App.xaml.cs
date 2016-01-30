7461870aa94225f97bfa093871dafe28677b6ca2
Vladislav <v.d.gushin@live.ru>
2016-01-30 14:13:12 +0300

    Update App.xaml.cs

--- TestUniversalApp/App.xaml.cs
+++ TestUniversalApp/App.xaml.cs
@@ -49,23 +49,18 @@ namespace TestUniversalApp
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
-
-            Frame rootFrame = Window.Current.Content as Frame;
-
+            Frame rootFrame = Window.Current.Content; //as //Frame;
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
             if (rootFrame == null)
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-
                 rootFrame.NavigationFailed += OnNavigationFailed;
-
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
                 }
-
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
