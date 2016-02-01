f725fefddd0dbe1e26ace7c29d916d242a049f7b
Vladislav <v.d.gushin@live.ru>
2016-02-01 12:40:35 +0300

    develop change
    make fix

--- TestUniversalApp/App.xaml.cs
+++ TestUniversalApp/App.xaml.cs
@@ -50,8 +50,7 @@ namespace TestUniversalApp
             }
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
+        
             if (rootFrame == null)
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
